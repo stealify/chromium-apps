277d62ffddda9ab9c220acadaee4cb533ac7ec3d
Frank Lemanschik <frank@lemanschik.com>
2022-10-06 10:20:32 +0200

    Create apps.d.ts
    Signed-off-by: Frank Lemanschik <frank@lemanschik.com>

---
+++ types/apps.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Chromium Apps Api (MOJO & Devtools driven)
+ * Replacement for Electron & NWJS
+ * Implements the core functions needed for both API's 
+ * Implements the puppeteer and playwright api's
+ * Aims to be the new source for the devtools-protocol definitions. 
+ * Is related to Project fugu proposals:
+ *  - MOJO Browser API Access
+ *  - Capability Based MOJO Access Protocol passing Mojo Components (Fuchsia OS)
+ */
